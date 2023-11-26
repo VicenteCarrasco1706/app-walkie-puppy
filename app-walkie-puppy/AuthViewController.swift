8abe9e5aaf05e41daa7f60a951f2a45b78be152f
DAMII <damii@MACBE207-ST02.local>
2023-11-26 11:27:57 -0500

    Registrar Actializado

--- app-walkie-puppy/AuthViewController.swift
+++ app-walkie-puppy/AuthViewController.swift
@@ -15,25 +15,12 @@ class AuthViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
-    @IBOutlet weak var singUpButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordTextField.isSecureTextEntry = true
         // Do any additional setup after loading the view.
     }
 
-    
-    
-    @IBAction func singUpButtonAction(_ sender: Any) {
-        if let email = emailTextField.text, let password = passwordTextField.text{
-            Auth.auth().createUser(withEmail: email, password: password){ (result, error) in
-                if let error = error{
-                    print("Error durante la autenticacion: \(error.localizedDescription)")
-            } else {
-                self.transitionToLoginViewController()
-            }
-        }
-    }
-    }
     
     @IBAction func logInButtonAction(_ sender: Any) {
         if let email = emailTextField.text, let password = passwordTextField.text{
@@ -53,10 +40,4 @@ class AuthViewController: UIViewController {
             self.navigationController?.pushViewController(inicioViewController, animated: true)
         }
         }
-    
-    func transitionToLoginViewController(){
-        if let loginViewController = self.storyboard?.instantiateViewController(identifier: "LoginViewController"){
-        self.navigationController?.pushViewController(loginViewController, animated: true)
-    }
-    }
 }
---
+++ app-walkie-puppy/RegisterUsuarioViewController.swift
@@ -0,0 +1,60 @@
+//
+//  RegisterUsuarioViewController.swift
+//  app-walkie-puppy
+//
+//  Created by DAMII on 26/11/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class RegisterUsuarioViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var singUpButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func singUpButtonAction(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password){ (result, error) in
+                if let error = error{
+                    print("Error durante la autenticacion: \(error.localizedDescription)")
+            } else {
+                self.transitionToLoginViewController()
+            }
+        }
+    }
+    }
+    
+    
+                
+        func transitionToInicioViewController(){
+            if let inicioViewController = self.storyboard?.instantiateViewController(identifier: "InicioViewController"){
+            self.navigationController?.pushViewController(inicioViewController, animated: true)
+        }
+        }
+    
+    func transitionToLoginViewController(){
+        if let loginViewController = self.storyboard?.instantiateViewController(identifier: "LoginViewController"){
+        self.navigationController?.pushViewController(loginViewController, animated: true)
+    }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
